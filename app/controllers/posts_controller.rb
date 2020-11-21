0e1c095e0925f169fd6b174b932a9f12dae3602f
sayahaya <sayahaya1129@gmail.com>
2020-11-21 16:34:57 +0900

    first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,11 @@ class PostsController < ApplicationController
   end
 
   def show
+   @post = Post.find(params[:id])
   end
 
   def new
-   @post = Post.all
+   @post = Post.new
   end
 
   def edit
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_014217) do
+ActiveRecord::Schema.define(version: 2020_11_21_054912) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
